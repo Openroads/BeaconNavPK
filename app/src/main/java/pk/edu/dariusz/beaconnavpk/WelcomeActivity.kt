42fd6f9dfdf5dcf72fa119d5e6d8c84d25e27bcb
daren <daren22@o2.pl>
2019-02-02 21:44:15 +0100

    Navigation activity and layout on example navigation map for flat

---
+++ app/src/main/java/pk/edu/dariusz/beaconnavpk/WelcomeActivity.kt
@@ -0,0 +1,19 @@
+package pk.edu.dariusz.beaconnavpk
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_welcome.*
+
+class WelcomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+
+        start_button.setOnClickListener {
+            val intent = Intent(this, NavigationActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
